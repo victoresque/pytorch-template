da6f6f74b57467868e407c2e79ab62408a3c6b7e
Victor Huang <vichuang1997@gmail.com>
2018-04-18 16:09:04 +0800

    Add default value config=None in MnistModel

--- model/model.py
+++ model/model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class MnistModel(BaseModel):
-    def __init__(self, config):
+    def __init__(self, config=None):
         super(MnistModel, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
