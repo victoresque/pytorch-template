bdd5acdc9ce14a3e764854c178ce920448cc5107
SunQ <sunq0313@gmail.com>
2018-08-25 03:13:24 +0900

    split valid_set without list packing

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -1,100 +1,67 @@
-from copy import copy
 import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data.dataloader import default_collate
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
-class BaseDataLoader:
+class BaseDataLoader(DataLoader):
     """
     Base class for all data loaders
     """
-    def __init__(self, config):
+    def __init__(self, dataset, config, collate_fn=default_collate):
+        self.dataset = dataset
         self.config = config
+        self.collate_fn = collate_fn
+
         self.batch_size = config['data_loader']['batch_size']
+        self.validation_split = config['validation']['validation_split']
         self.shuffle = config['data_loader']['shuffle']
+        
         self.batch_idx = 0
+        self._n_samples = len(self.dataset)
 
-    def __iter__(self):
-        """
-        :return: Iterator
-        """
-        assert self.__len__() > 0
-        self.batch_idx = 0
-        if self.shuffle:
-            self._shuffle_data()
-        return self
-
-    def __next__(self):
-        """
-        :return: Next batch
-        """
-        packed = self._pack_data()
-        if self.batch_idx < self.__len__():
-            batch = packed[self.batch_idx * self.batch_size:(self.batch_idx + 1) * self.batch_size]
-            self.batch_idx = self.batch_idx + 1
-            return self._unpack_data(batch)
+        if self.validation_split == 0.0:
+            self.sampler, self.valid_sampler = None, None
         else:
-            raise StopIteration
+            self.sampler, self.valid_sampler = self._split_sampler(self.validation_split)
+            # turn off shuffle option which is mutually exclusive with sampler
+            self.shuffle = False
+
+        self.init_kwargs = {
+            'dataset': self.dataset,
+            'batch_size': self.batch_size,
+            'shuffle': self.shuffle,
+            'collate_fn': self.collate_fn
+            }
+        super(BaseDataLoader, self).__init__(sampler=self.sampler, **self.init_kwargs)
 
     def __len__(self):
         """
         :return: Total number of batches
         """
-        return self._n_samples() // self.batch_size
+        return self._n_samples // self.batch_size
 
-    def _n_samples(self):
-        """
-        :return: Total number of samples
-        """
-        return NotImplementedError
+    def _split_sampler(self, split):
+        assert split > 0.0
+        idx_full = np.arange(self._n_samples)
 
-    def _pack_data(self):
-        """
-        Pack all data into a list/tuple/ndarray/...
+        # TODO: make sure that this seed does not influence other sampling
+        np.random.seed(0) 
+        np.random.shuffle(idx_full)
 
-        :return: Packed data in the data loader
-        """
-        return NotImplementedError
+        len_valid = int(self._n_samples * split)
 
-    def _unpack_data(self, packed):
-        """
-        Unpack packed data (from _pack_data())
-
-        :param packed: Packed data
-        :return: Unpacked data
-        """
-        return NotImplementedError
-
-    def _update_data(self, unpacked):
-        """
-        Update data member in the data loader
-
-        :param unpacked: Unpacked data (from _update_data())
-        """
-        return NotImplementedError
-
-    def _shuffle_data(self):
-        """
-        Shuffle data members in the data loader
-        """
-        packed = self._pack_data()
-        rand_idx = np.random.permutation(len(packed))
-        packed = [packed[i] for i in rand_idx]
-        self._update_data(self._unpack_data(packed))
-
+        valid_idx = idx_full[0:len_valid]
+        train_idx = np.delete(idx_full, np.arange(0, len_valid))
+        
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+        self._n_samples = len(train_idx)
+        return train_sampler, valid_sampler
+        
     def split_validation(self):
-        """
-        Split validation data from data loader based on self.config['validation']
-        """
-        validation_split = self.config['validation']['validation_split']
-        shuffle = self.config['validation']['shuffle']
-        if validation_split == 0.0:
+        if self.valid_sampler is None:
             return None
-        if shuffle:
-            self._shuffle_data()
-        valid_data_loader = copy(self)
-        split = int(self._n_samples() * validation_split)
-        packed = self._pack_data()
-        train_data = self._unpack_data(packed[split:])
-        val_data = self._unpack_data(packed[:split])
-        valid_data_loader._update_data(val_data)
-        self._update_data(train_data)
-        return valid_data_loader
+        else:
+            return DataLoader(sampler=self.valid_sampler, **self.init_kwargs)
+    
