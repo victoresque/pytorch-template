6f78f1dff5e16a32e8c3b71d06c5dbca7bdbf704
borgesan <borgesan@ifi.uio.no>
2018-09-12 13:55:41 +0200

    Created 'get_xyz' fns, replaced 'import *' in 'train.py'.

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,12 @@
 import torch.nn.functional as F
 
+def get_loss_function(loss_fn_name):
+    try:
+        loss_fn = eval(loss_fn_name)
+    except NameError as e:
+        raise NameError(f"Loss function '{loss_fn_name}' not found.")
+
+    return loss_fn
 
 def my_loss(y_input, y_target):
     return F.nll_loss(y_input, y_target)
