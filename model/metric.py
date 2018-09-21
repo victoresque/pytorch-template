b5fe4add701a4d510571f5308e26d4fa5ec8a176
SunQ <sunq0313@gmail.com>
2018-09-21 09:01:03 +0900

    add no_grad flag for metric computation

--- model/metric.py
+++ model/metric.py
@@ -2,16 +2,18 @@ import torch
 
 
 def my_metric(output, target):
-    pred = torch.argmax(output, dim=1)
-    assert pred.shape[0] == len(target)
-    correct = 0
-    correct += torch.sum(pred == target).item()
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
     return correct / len(target)
 
 def my_metric2(output, target, k=3):
-    pred = torch.topk(output, k, dim=1)[1]
-    assert pred.shape[0] == len(target)
-    correct = 0
-    for i in range(k):
-        correct += torch.sum(pred[:, i] == target).item()
+    with torch.no_grad():
+        pred = torch.topk(output, k, dim=1)[1]
+        assert pred.shape[0] == len(target)
+        correct = 0
+        for i in range(k):
+            correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
