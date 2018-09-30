df5e94940a155d746a74f08b67a700904bc2edef
SunQ <sunq0313@gmail.com>
2018-10-01 08:46:04 +0900

    add test.py

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -6,12 +6,12 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle, validation_split):
+    def __init__(self, data_dir, batch_size, shuffle, validation_split, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
             ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=True, download=True, transform=trsfm)
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super(MnistDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split)
         
---
+++ test.py
@@ -0,0 +1,75 @@
+import argparse
+import torch
+from tqdm import tqdm
+import data_loader.data_loaders as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+
+def get_instance(module, name, config, *args):
+    return getattr(module, config[name]['type'])(*args, **config[name]['args'])
+
+def main(config, resume):
+    # setup data_loader instances
+    data_loader = getattr(module_data, config['data_loader']['type'])(
+        config['data_loader']['args']['data_dir'],
+        batch_size=512,
+        shuffle=False,
+        validation_split=0.0,
+        training=False
+    )
+
+    # build model architecture
+    model = get_instance(module_arch, 'arch', config)
+    model.summary()
+
+    # get function handles of loss and metrics
+    loss_fn = getattr(module_loss, config['loss'])
+    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
+
+    # load state dict
+    checkpoint = torch.load(resume)
+    state_dict = checkpoint['state_dict']
+    model.load_state_dict(state_dict)
+
+    # prepare model for testing
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    model.eval()
+
+    total_loss = 0.0
+    total_metrics = torch.zeros(len(metric_fns))
+
+    with torch.no_grad():
+        for i, (data, target) in enumerate(tqdm(data_loader)):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            #
+            # save sample images, or do something with output here
+            #
+            
+            # computing loss, metrics on test set
+            loss = loss_fn(output, target)
+            batch_size = data.shape[0]
+            total_loss += loss.item() * batch_size
+            for i, metric in enumerate(metric_fns):
+                total_metrics[i] += metric(output, target) * batch_size
+
+    n_samples = len(data_loader.sampler)
+    log = {'loss': total_loss / n_samples}
+    log.update({met.__name__ : total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)})
+    print(log)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch Template')
+
+    parser.add_argument('-r', '--resume', default=None, type=str,
+                           help='path to latest checkpoint (default: None)')
+
+    args = parser.parse_args()
+
+    if args.resume:
+        config = torch.load(args.resume)['config']
+
+    main(config, args.resume)
