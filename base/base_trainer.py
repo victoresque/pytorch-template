54489c707b3ac113a4869e7c15e1ed54d40d5d30
SunQ <sunq0313@gmail.com>
2018-10-02 07:06:53 +0900

    update comments, delete outdated fix of optim loading
    pytorch had a bug, which yielded cuda tensor type error when loading
    Adam optimizer with amsgrad option.
    Deleted lines for handling that, since that bug no longer exists

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -142,23 +142,18 @@ class BaseTrainer:
         self.start_epoch = checkpoint['epoch'] + 1
         self.monitor_best = checkpoint['monitor_best']
 
-        # load architecture from checkpoint.
-        if self.config['arch'] != checkpoint['arch']:
-            self.logger.warning('Architecture setting given in config file is different from that of checkpoint. ' + \
-                                'This may produce an exception while loading state_dict.')
+        # load architecture params from checkpoint.
+        if checkpoint['config']['arch'] != self.config['arch']:
+            self.logger.warning('Warning: Architecture configuration given in config file is different from that of checkpoint. ' + \
+                                'This may yield an exception while state_dict is being loaded.')
         self.model.load_state_dict(checkpoint['state_dict'])
 
-        # load optimizer from checkpoint.
-        if checkpoint['config']['optimizer']['type'] == self.config['optimizer']['type']:
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
-            if self.with_cuda:
-                for state in self.optimizer.state.values():
-                    for k, v in state.items():
-                        if isinstance(v, torch.Tensor):
-                            state[k] = v.cuda(self.device)
-        else:
-            self.logger.warning('Optimizer type given in config file is different from that of checkpoint. ' + \
+        # load optimizer state from checkpoint only when optimizer type is not changed. 
+        if checkpoint['config']['optimizer']['type'] != self.config['optimizer']['type']:
+            self.logger.warning('Warning: Optimizer type given in config file is different from that of checkpoint. ' + \
                                 'Optimizer parameters not being resumed.')
-
+        else:
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+    
         self.train_logger = checkpoint['logger']
         self.logger.info("Checkpoint '{}' (epoch {}) loaded".format(resume_path, self.start_epoch))
