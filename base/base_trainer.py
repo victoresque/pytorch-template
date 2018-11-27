df5c14437e4c5d2d3538678c46a043f00018dc74
SunQ <sunq0313@gmail.com>
2018-11-27 16:33:59 +0900

    change config file, base_trainer layout

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -25,31 +25,33 @@ class BaseTrainer:
         self.loss = loss
         self.metrics = metrics
         self.optimizer = optimizer
-
-        self.epochs = config['trainer']['epochs']
-        self.save_freq = config['trainer']['save_freq']
-        self.verbosity = config['trainer']['verbosity']
-
         self.train_logger = train_logger
 
+        cfg_trainer = config['trainer']
+        self.epochs = cfg_trainer['epochs']
+        self.save_period = cfg_trainer['save_period']
+        self.verbosity = cfg_trainer['verbosity']
+        self.monitor = cfg_trainer.get('monitor', 'off')
+
         # configuration to monitor model performance and save best
-        self.monitor = config['trainer']['monitor']
-        self.monitor_mode = config['trainer']['monitor_mode']
+        if self.monitor == 'off':
+            self.mnt_mode = 'off'
+            self.mnt_best = 0
+        else:
+            self.mnt_mode, self.mnt_metric = self.monitor.split()
+            assert self.mnt_mode in ['min', 'max']
 
-        self.early_stop = config['trainer']['early_stop']
-        if self.early_stop <= 0 or self.early_stop == None:
-            self.early_stop = math.inf
-
-        assert self.monitor_mode in ['min', 'max', 'off']
-        self.monitor_best = math.inf if self.monitor_mode == 'min' else -math.inf
+            self.mnt_best = math.inf if self.mnt_mode == 'min' else -math.inf
+            self.early_stop = cfg_trainer.get('early_stop', math.inf)
+        
         self.start_epoch = 1
 
         # setup directory for checkpoint saving
         start_time = datetime.datetime.now().strftime('%m%d_%H%M%S')
-        self.checkpoint_dir = os.path.join(config['trainer']['save_dir'], config['name'], start_time)
+        self.checkpoint_dir = os.path.join(cfg_trainer['save_dir'], config['name'], start_time)
         # setup visualization writer instance
-        writer_dir = os.path.join(config['visualization']['log_dir'], config['name'], start_time)
-        self.writer = WriterTensorboardX(writer_dir, self.logger, config['visualization']['tensorboardX'])
+        writer_dir = os.path.join(cfg_trainer['log_dir'], config['name'], start_time)
+        self.writer = WriterTensorboardX(writer_dir, self.logger, cfg_trainer['tensorboardX'])
 
         # Save configuration file into checkpoint directory:
         ensure_dir(self.checkpoint_dir)
@@ -101,25 +103,31 @@ class BaseTrainer:
 
             # evaluate model performance according to configured metric, save best checkpoint as model_best
             best = False
-            if self.monitor_mode != 'off':
+            if self.mnt_mode != 'off':
                 try:
-                    if  (self.monitor_mode == 'min' and log[self.monitor] < self.monitor_best) or\
-                        (self.monitor_mode == 'max' and log[self.monitor] > self.monitor_best):
-                        self.monitor_best = log[self.monitor]
-                        not_improved_count = 0
-                        best = True
+                    # check whether model performance improved or not, according to specified metric(mnt_metric)
+                    improved = (self.mnt_mode == 'min' and log[self.mnt_metric] < self.mnt_best) or \
+                               (self.mnt_mode == 'max' and log[self.mnt_metric] > self.mnt_best)
                 except KeyError:
-                    self.logger.warning("Warning: Metric '{}' is not found. Model performance monitoring is disabled.".format(self.monitor))
-                    self.monitor_mode = 'off'
+                    self.logger.warning("Warning: Metric '{}' is not found. Model performance monitoring is disabled.".format(self.mnt_metric))
+                    self.mnt_mode = 'off'
+                    improved = False
                     not_improved_count = 0
 
-            if epoch % self.save_freq == 0:
+                if improved:
+                    self.mnt_best = log[self.mnt_metric]
+                    not_improved_count = 0
+                    best = True
+                else:
+                    not_improved_count += 1
+
+                if not_improved_count > self.early_stop:
+                    self.logger.info("Validation performance didn\'t improve for {} epochs. Training stops.".format(self.early_stop))
+                    break
+
+            if epoch % self.save_period == 0:
                 self._save_checkpoint(epoch, save_best=best)
             
-            if not_improved_count >= self.early_stop:
-                self.logger.info("Validation performance didn\'t improve for {} epochs. Training stops.".format(self.early_stop))
-                break
-            not_improved_count += 1
 
     def _train_epoch(self, epoch):
         """
@@ -144,7 +152,7 @@ class BaseTrainer:
             'logger': self.train_logger,
             'state_dict': self.model.state_dict(),
             'optimizer': self.optimizer.state_dict(),
-            'monitor_best': self.monitor_best,
+            'monitor_best': self.mnt_best,
             'config': self.config
         }
         filename = os.path.join(self.checkpoint_dir, 'checkpoint-epoch{}.pth'.format(epoch))
@@ -164,7 +172,7 @@ class BaseTrainer:
         self.logger.info("Loading checkpoint: {} ...".format(resume_path))
         checkpoint = torch.load(resume_path)
         self.start_epoch = checkpoint['epoch'] + 1
-        self.monitor_best = checkpoint['monitor_best']
+        self.mnt_best = checkpoint['monitor_best']
 
         # load architecture params from checkpoint.
         if checkpoint['config']['arch'] != self.config['arch']:
