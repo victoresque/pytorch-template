d133dc604508ff025c261af51ea194e2f8007bb3
SunQ <SunQ0313@gmail.com>
2019-03-05 11:20:05 +0900

    fix warning on tensorboardx import error

--- utils/visualization.py
+++ utils/visualization.py
@@ -8,7 +8,7 @@ class WriterTensorboardX():
             log_path = writer_dir
             try:
                 self.writer = importlib.import_module('tensorboardX').SummaryWriter(log_path)
-            except ModuleNotFoundError:
+            except ImportError:
                 message = "Warning: TensorboardX visualization is configured to use, but currently not installed on this machine. " + \
                           "Please install the package by 'pip install tensorboardx' command or turn off the option in the 'config.json' file."
                 logger.warning(message)
