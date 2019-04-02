7cc408c38d91992aa2344c9bdfb4453b9040d915
SunQ <SunQ0313@gmail.com>
2019-04-02 16:36:12 +0900

    add some default arguments for data_loader

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -6,7 +6,7 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
