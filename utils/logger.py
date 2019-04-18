736b854272a35d103a5a8f3954487bd7eecad3b7
SunQ <SunQ0313@gmail.com>
2019-04-18 17:24:08 +0900

    change logger config from yaml to json

--- utils/logger.py
+++ utils/logger.py
@@ -1,18 +1,16 @@
-import yaml
 import logging
 import logging.config
 from pathlib import Path
+from .util import read_json
 
 
-def setup_logging(save_dir, log_config='logger/logger_config.yaml', default_level=logging.INFO):
+def setup_logging(save_dir, log_config='utils/logger_config.json', default_level=logging.INFO):
     """
     Setup logging configuration
     """
     log_config = Path(log_config)
     if log_config.is_file():
-        with log_config.open('rt') as f:
-            config = yaml.safe_load(f.read())
-
+        config = read_json(log_config)
         # modify logging paths based on run config
         for _, handler in config['handlers'].items():
             if 'filename' in handler:
