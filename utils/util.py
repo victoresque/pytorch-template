36ffcb37aa4b736ce26c69c0574313ec3614ae00
SunQ <SunQ0313@gmail.com>
2019-06-10 10:56:01 +0900

    remove Timer class

--- utils/util.py
+++ utils/util.py
@@ -1,6 +1,5 @@
 import json
 from pathlib import Path
-from datetime import datetime
 from itertools import repeat
 from collections import OrderedDict
 
@@ -22,16 +21,3 @@ def inf_loop(data_loader):
     ''' wrapper function for endless data loader. '''
     for loader in repeat(data_loader):
         yield from loader
-
-class Timer:
-    def __init__(self):
-        self.cache = datetime.now()
-
-    def check(self):
-        now = datetime.now()
-        duration = now - self.cache
-        self.cache = now
-        return duration.total_seconds()
-
-    def reset(self):
-        self.cache = datetime.now()
