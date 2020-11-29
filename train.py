3c2bbc678a712723047042a831c6598dcedc9607
SunQ <SunQ0313@gmail.com>
2020-11-30 02:07:45 +0900

    fix error
    using getitem instead of getattr

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ def main(config):
     logger.info(model)
 
     # prepare for (multi-device) GPU training
-    device, device_ids = prepare_device(config.n_gpu)
+    device, device_ids = prepare_device(config['n_gpu'])
     model = model.to(device)
     if len(device_ids) > 1:
         model = torch.nn.DataParallel(model, device_ids=device_ids)
