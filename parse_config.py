5abc7e05b180cf241411ea9f030bf7f0370eb53e
Tackoil <santoin351@outlook.com>
2022-10-25 10:48:11 +0800

    feat: support `in` operation in ConfigParser

--- parse_config.py
+++ parse_config.py
@@ -111,6 +111,10 @@ class ConfigParser:
         """Access items like ordinary dict."""
         return self.config[name]
 
+    def __contains__(self, name):
+        """Use in operation like ordinary dict."""
+        return name in self.config
+
     def get_logger(self, name, verbosity=2):
         msg_verbosity = 'verbosity option {} is invalid. Valid options are {}.'.format(verbosity, self.log_levels.keys())
         assert verbosity in self.log_levels, msg_verbosity
